3114c34c3372b3b4fc7cc7402bd99e287ba05cb5
Ugo Ezekwem <the_zex@yahoo.com>
2018-05-22 17:15:59 +0100

    fixed user show page and updated dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -46,6 +46,7 @@
             <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
               <span class="count_top"><i class="fa fa-user"></i> Total Reports</span>
               <div class="count">{{ $reports->count() }}</div>
+              <span class="count_bottom"><i >{{ $reports->where('approval_status', 0)->count() }}</i> pending</span>
               <span class="count_bottom"><i >{{ $reports->where('approval_status', 1)->count() }}</i> approved</span>
               <span class="count_bottom"><i >{{ $reports->where('approval_status', 2)->count() }}</i> rejected</span>
             </div> 
